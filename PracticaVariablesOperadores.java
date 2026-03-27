8bffd27c33d372ca191cfc9e14e761928bca20b0
Giuliana Victoria <139921890+giulianaarganaraz1@users.noreply.github.com>
2026-03-27 08:40:16 -0300

    Create PracticaVariablesOperadores.java

---
+++ PracticaVariablesOperadores.java
@@ -0,0 +1,9 @@
+public class variablesOperadores {
+    public static void main(String[] args){
+        int Presiogomita = 30 ;
+        int cantidadCliente= 4 ; 
+        int Total= Presiogomita * cantidadCliente ;
+        System.out.println(Total);
+}
+}
+    
